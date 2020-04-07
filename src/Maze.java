639f5de147536ea87d130a56a193a6aae3dfd9bd
JacobSPalmer <jpalmer3@luc.edu>
2020-04-07 16:00:15 -0700

    commit 1

--- src/Maze.java
+++ src/Maze.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Maze {
 
     private final char[][][] maze;
-    private final char WALL = '*', PATH = '.', VISITED = '+', DEADEND = '-', START = '0';
+    private final char WALL = '*', PATH = '.', VISITED = '+', DEADEND = '-', START = '0', MOVEUP = '^', MOVEDOWN = '!';
     private int TotalRow;
     private int TotalCol;
     private int TotalLevel;
@@ -20,7 +20,7 @@ public class Maze {
     public void printMaze(){
         for(int i1 = 0; i1 < maze.length; i1++){
             System.out.println("Level " + i1);
-            for(int i2 = 0; i2itg < maze[0].length; i2++){
+            for(int i2 = 0; i2 < maze[0].length; i2++){
                 System.out.println(maze[i1][i2]);
             }
         }
